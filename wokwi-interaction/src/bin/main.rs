f110de09e5844b6b7fd83257a23b58ac5ad9a7e4
Ezra Maccabee <von.akimov@gmail.com>
2025-12-02 18:53:29 +0300

    # Совместная работа с Wokwi
    Эмуляция приложения в Wokwi:
    "Подсчет количества нажатий кнопки (IRQ), изменение состояния светодиода"

--- wokwi-interaction/src/bin/main.rs
+++ wokwi-interaction/src/bin/main.rs
@@ -6,14 +6,33 @@
     holding buffers for the duration of a data transfer."
 )]
 
+use core::cell::{Cell, RefCell};
 use esp_backtrace as _;
-use esp_hal::clock::CpuClock;
-use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
-use log::info;
 
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+use critical_section::Mutex;
+use esp_hal::clock::CpuClock;
+use esp_hal::gpio::{Event, Input, InputConfig, Io, Level, Output, OutputConfig};
+use esp_hal::{handler, main};
+use log::{info, warn};
+
+static BUTTON: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
+static BTN_PRESSED: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
+
+#[handler]
+fn handler() {
+    critical_section::with(|cs| {
+        // Очистка флага прерывания
+        BUTTON
+            .borrow_ref_mut(cs)
+            .as_mut()
+            .unwrap()
+            .clear_interrupt();
+
+        // Установка флага
+        BTN_PRESSED.borrow(cs).set(true);
+    });
+}
+
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]
@@ -23,13 +42,36 @@ fn main() -> ! {
     esp_println::logger::init_logger_from_env();
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
-    let _peripherals = esp_hal::init(config);
+    let peripherals = esp_hal::init(config);
 
+    let mut io = Io::new(peripherals.IO_MUX);
+
+    // Установка обработчика прерываний
+    io.set_interrupt_handler(handler);
+    io.set_interrupt_priority(esp_hal::interrupt::Priority::Priority1);
+
+    let mut led = Output::new(peripherals.GPIO7, Level::Low, OutputConfig::default());
+
+    let mut button = Input::new(
+        peripherals.GPIO9,
+        InputConfig::default().with_pull(esp_hal::gpio::Pull::Up),
+    );
+
+    // Запуск обработки прерываний, передача handle button в глобальный контекст BUTTON для работы в другом потоке выполнения
+    button.listen(Event::FallingEdge);
+    critical_section::with(|cs| BUTTON.borrow_ref_mut(cs).replace(button));
+
+    let mut count = 0;
+
+    info!("Main thread has started...");
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        critical_section::with(|cs| {
+            if BTN_PRESSED.borrow(cs).get() {
+                BTN_PRESSED.borrow(cs).set(false);
+                count += 1;
+                warn!("Button pressed {} times", count);
+                led.toggle();
+            }
+        });
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
 }
